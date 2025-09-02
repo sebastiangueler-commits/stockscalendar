9a8888133c9658ca2ad58e0e39770c7a7489c70f
Magic Stocks Calendar <admin@magicstocks.com>
2025-09-02 18:11:28 -0300

    Simplify Netlify configuration and add test function

---
+++ frontend/functions/test-simple.js
@@ -0,0 +1,14 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      success: true,
+      message: 'Functions are working!',
+      time: new Date().toISOString()
+    })
+  };
+};
